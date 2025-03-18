9108feccf4acfca5f43a40417f0f0402998945ed
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-03-18 13:44:55 +0600

    feat: add initial crud of blogs
    refs: #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core';
+import { BlogsModule } from './blogs/blogs.module';
 
 
 @Module({
   imports: [
       CoreModule,
+      BlogsModule,
   ],
 })
 export class AppModule {}
