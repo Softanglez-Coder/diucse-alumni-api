1b55cbdacae6a8c9dd92d43e2717b41c9979514f
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-03-21 21:49:40 +0600

    feat: added CRUD functionality and database integration

--- src/blogs/entities/blog.entity.ts
+++ src/blogs/entities/blog.entity.ts
@@ -1,49 +1,3 @@
-// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import { Document } from 'mongoose';
-
-// export type BlogDocument = Blog & Document;
-
-// @Schema()
-// export class Blog {
-//   @Prop({ required: true })
-//   id: string;
-
-//   @Prop({ required: true })
-//   title: string;
-
-//   @Prop({ required: true })
-//   content: string;
-
-//   @Prop({ type: Object })
-//   author: { id: string; name: string };
-
-//   @Prop({ required: true })
-//   createdAt: string;
-
-//   @Prop({ type: Object })
-//   createdBy: { id: string; name: string };
-
-//   @Prop({ type: String, default: null })
-//   modifiedAt?: string | null;
-
-//   @Prop({ type: Object, default: null })
-//   modifiedBy?: { id: string; name: string } | null;
-
-//   @Prop({ type: String, default: null })
-//   deletedAt?: string | null;
-
-//   @Prop({ type: Object, default: null })
-//   deletedBy?: { id: string; name: string } | null;
-// }
-
-// export const BlogSchema = SchemaFactory.createForClass(Blog);
-
-
-
-
-
-
-// //mongodb er jonno 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
@@ -83,7 +37,3 @@ export class Blog {
 }
 
 export const BlogSchema = SchemaFactory.createForClass(Blog);
-
-
-
-
