f7ba3f94932944f45a96f46ffbbf02d9c1921db9
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-03-28 01:48:49 +0600

    fix: resolved merge conflicts in package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CoreModule } from './core';
-<<<<<<< HEAD
 import { MembershipModule } from './membership/membership.module';
 import { ConfigModule } from '@nestjs/config';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot(), 
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y15rh.mongodb.net/diucseapi?retryWrites=true&w=majority`,
-    ),
-    CoreModule,
-    MembershipModule,
-=======
 import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
-      CoreModule,
-      UploadModule
->>>>>>> origin/dev
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y15rh.mongodb.net/diucseapi?retryWrites=true&w=majority`,
+    ),//mongodb connect 
+    CoreModule,
+    MembershipModule,
+    UploadModule, 
   ],
 })
 export class AppModule {}
