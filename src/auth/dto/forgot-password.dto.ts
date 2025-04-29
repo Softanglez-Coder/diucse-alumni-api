e85388ff313654bebd797977143cf1257c558e15
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-04-29 14:11:24 +0600

    fix: some error

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -1,12 +1,13 @@
 import { IsNotEmpty } from 'class-validator';
 
 export class ForgotPasswordDto {
-    static email(email: any, token: string) {
-        throw new Error('Method not implemented.');
-    }
-    email(email: any, token: string) {
-        throw new Error('Method not implemented.');
-    }
-    @IsNotEmpty()
-    username: string;
+  static email(_email: any, _token: string) {
+    throw new Error('Method not implemented.');
+  }
+  email(_email: any, _token: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  @IsNotEmpty()
+  username: string;
 }
