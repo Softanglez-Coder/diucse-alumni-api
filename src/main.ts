503310ab38a2cc966c89995fbc2db36ac5c2e0bd
Md. Ashikur Rahman Khan <ashik.est04@gmail.com>
2025-05-01 19:19:13 +0600

    feat(cache): implement Redis-based caching for all GET APIs and invalidate on data changes #30

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Reflector } from '@nestjs/core';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cacheManager = app.get<Cache>(CACHE_MANAGER);
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new CacheInterceptor(cacheManager, reflector));
   const port = process.env.PORT ?? 3000;
   await app
     .listen(port)
@@ -10,3 +18,5 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+
